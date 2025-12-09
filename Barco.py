5bdfcf99645d646429916cb7f29fac952ec77dba
Isaac <irodriguezsolla@danielcastelao.org>
2025-12-09 13:58:03 +0100

    Misión Inicial: Establecida la estructura de la Flota Base

---
+++ Barco.py
@@ -0,0 +1,28 @@
+class Barco:
+   def __init__(self, nombre, longitud):
+       self.nombre = nombre
+       self.longitud = longitud
+       self.golpes_recibidos = 0
+
+
+   def recibir_impacto(self):
+       self.golpes_recibidos += 1
+
+
+   def esta_hundido(self):
+       return self.golpes_recibidos == self.longitud
+
+if __name__ == "__main__":
+   submarino = Barco("Submarino", 1)
+   buque = Barco("Buque", 3)
+
+
+   submarino.recibir_impacto()
+   print(submarino.esta_hundido())
+
+
+   buque.recibir_impacto()
+   buque.recibir_impacto()
+   print(buque.esta_hundido())
+   buque.recibir_impacto()
+   print(buque.esta_hundido())
