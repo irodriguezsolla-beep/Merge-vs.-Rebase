dacfdbc0aab639a447e5d53ac6e26771381aa03f
andre <aperazanascimento@danielcastelao.org>
2025-12-09 13:58:37 +0100

    Tablero U2: Implementación de la cuadrícula de ataque

---
+++ Tablero.py
@@ -0,0 +1,13 @@
+class Tablero:
+    def __init__(self, tamano):
+        self.dimensiones = (tamano, tamano)
+        self.casillas = [[0 for _ in range(tamano)] for _ in range(tamano)]
+
+    def mostrar_tablero(self):
+        for fila in self.casillas:
+            print(fila)
+
+
+if __name__ == "__main__":
+    tablero = Tablero(5)
+    tablero.mostrar_tablero()
